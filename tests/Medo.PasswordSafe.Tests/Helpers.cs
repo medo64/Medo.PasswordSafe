27b247c69607deee92c206ba8ff4901e8ad78eb1
Josip Medved <jmedved@jmedved.com>
2024-07-02 19:28:38 -0700

    Refactored to compile into multiple frameworks only when publishing

--- tests/Medo.PasswordSafe.Tests/Helpers.cs
+++ tests/Medo.PasswordSafe.Tests/Helpers.cs
@@ -8,6 +8,7 @@ internal static class Helpers {
 
     public static MemoryStream GetResourceStream(string fileName) {
         var resStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Tests.Resources." + fileName);
+        if (resStream == null) { resStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Tests." + fileName); }
         var buffer = new byte[(int)resStream.Length];
         resStream.Read(buffer, 0, buffer.Length);
         return new MemoryStream(buffer) { Position = 0 };
